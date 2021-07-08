7f6cc60ead66bd912305770b009a248d62ba7617
Archana Maharjan <archana.maharjan@bentley.com>
2021-07-08 16:57:34 -0500

    adding Server Credentials in grpcClient options

--- src/grpcClient.options.ts
+++ src/grpcClient.options.ts
@@ -1,10 +1,27 @@
 import { ClientOptions, Transport } from '@nestjs/microservices';
+import { ServerCredentials } from 'grpc';
 import { join } from 'path';
+import * as fs from "fs";
 
 export const grpcClientOptions: ClientOptions = {
   transport: Transport.GRPC,
   options: {
     package: 'hero',
     protoPath: join(__dirname, './hero/hero.proto'),
+    // credentials: ServerCredentials.createInsecure()
+    credentials: ServerCredentials.createSsl(
+        fs.readFileSync(join(process.cwd(), 'certificates', 'server-cert.pem')),
+        [
+          {
+            private_key: fs.readFileSync(
+              join(process.cwd(), 'certificates', 'server-key.pem'),
+            ),
+            cert_chain: fs.readFileSync(
+              join(process.cwd(), 'certificates', 'ca-cert.pem'),
+            ),
+          },
+        ],
+        true,
+      )
   },
 };
