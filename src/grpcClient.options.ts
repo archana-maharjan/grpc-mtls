c625addde53eb07b7053ffaf1762a73f5971c4f1
Archana Maharjan <archana.maharjan@bentley.com>
2021-07-15 12:28:56 -0500

    separated server and client

--- src/grpcClient.options.ts
+++ src/grpcClient.options.ts
@@ -1,30 +1,26 @@
 import { ClientOptions, Transport } from '@nestjs/microservices';
 import { join } from 'path';
 import * as fs from "fs";
-import { credentials, ServerCredentials } from '@grpc/grpc-js';
+import { ServerCredentials } from '@grpc/grpc-js';
 
 export const grpcClientOptions: ClientOptions = {
   transport: Transport.GRPC,
   options: {
     package: 'hero',
     protoPath: join(__dirname, './hero/hero.proto'),
-    credentials: credentials.createSsl(
-      fs.readFileSync(join(process.cwd(), "certificates", "ca.cert")), 
-      fs.readFileSync(join(process.cwd(), "certificates", "service.key")), 
-      fs.readFileSync(join(process.cwd(), "certificates", "service.pem")))
-    // credentials: ServerCredentials.createSsl(
-    //     fs.readFileSync(join(process.cwd(), 'certificates', 'ca.cert')),
-    //     [
-    //       {
-    //         private_key: fs.readFileSync(
-    //           join(process.cwd(), 'certificates', 'service.key'),
-    //         ),
-    //         cert_chain: fs.readFileSync(
-    //           join(process.cwd(), 'certificates', 'service.pem'),
-    //         ),
-    //       },
-    //     ],
-    //     true,
-    //   )
+    credentials: ServerCredentials.createSsl(
+        fs.readFileSync(join(process.cwd(), 'certificates', 'ca.cert')),
+        [
+          {
+            private_key: fs.readFileSync(
+              join(process.cwd(), 'certificates', 'service.key'),
+            ),
+            cert_chain: fs.readFileSync(
+              join(process.cwd(), 'certificates', 'service.pem'),
+            ),
+          },
+        ],
+        true,
+      )
   },
 };
--- src/hero/hero.controller.ts
+++ src/hero/hero.controller.ts
@@ -1,53 +1,16 @@
-import { Controller, Get, Inject, OnModuleInit, Param } from '@nestjs/common';
+import { Controller} from '@nestjs/common';
 import {
-  ClientGrpc,
   GrpcStreamMethod,
 } from '@nestjs/microservices';
-import { Observable, ReplaySubject, Subject } from 'rxjs';
-import { toArray } from 'rxjs/operators';
+import { Observable, Subject } from 'rxjs';
 import { HeroById } from './interfaces/heroById.interface';
 import { Hero } from './interfaces/hero.interface';
-
-interface HeroService {
-  findOne(data: Observable<HeroById>): Observable<Hero>;
-  findMany(upstream: Observable<HeroById>): Observable<Hero>;
-}
-
 @Controller('hero')
-export class HeroController implements OnModuleInit {
+export class HeroController {
   private readonly items: Hero[] = [
     { id: 1, name: 'John' },
     { id: 2, name: 'Doe' },
   ];
-  private heroService: HeroService;
-
-  constructor(@Inject('HERO_PACKAGE') private readonly client: ClientGrpc) {}
-
-  onModuleInit() {
-    this.heroService = this.client.getService<HeroService>('HeroService');
-  }
-
-  @Get()
-  getMany(): Observable<Hero[]> {
-    const ids$ = new ReplaySubject<HeroById>();
-    ids$.next({ id: 1 });
-    ids$.next({ id: 2 });
-    ids$.complete();
-
-    const stream = this.heroService.findMany(ids$.asObservable());
-    return stream.pipe(toArray());
-  }
-
-  @Get(':id')
-  getById(@Param('id') id: number): Observable<Hero> {
-    // return this.heroService.findOne({ id: +id });
-    const idSubj = new ReplaySubject<HeroById>();
-    idSubj.next({ id });
-    idSubj.complete();
-
-    const stream = this.heroService.findOne(idSubj.asObservable());
-    return stream;
-  }
 
   @GrpcStreamMethod('HeroService')
   findOne(data$: Observable<HeroById>): Observable<Hero> {
