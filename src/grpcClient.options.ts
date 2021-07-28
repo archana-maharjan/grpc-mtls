52d11473a4c5a00353978cd6b57b4a1f92bb44bd
Archana Maharjan <archana.maharjan@bentley.com>
2021-07-28 15:04:10 -0500

    removed clientprovider

--- src/grpcClient.options.ts
+++ src/grpcClient.options.ts
@@ -1,6 +1,6 @@
 import { ClientOptions, Transport } from '@nestjs/microservices';
 import { join } from 'path';
-import * as fs from "fs";
+import * as fs from 'fs';
 import { ServerCredentials } from '@grpc/grpc-js';
 
 export const grpcClientOptions: ClientOptions = {
@@ -9,18 +9,18 @@ export const grpcClientOptions: ClientOptions = {
     package: 'hero',
     protoPath: join(__dirname, './hero/hero.proto'),
     credentials: ServerCredentials.createSsl(
-        fs.readFileSync(join(process.cwd(), 'certificates', 'ca.cert')),
-        [
-          {
-            private_key: fs.readFileSync(
-              join(process.cwd(), 'certificates', 'service.key'),
-            ),
-            cert_chain: fs.readFileSync(
-              join(process.cwd(), 'certificates', 'service.pem'),
-            ),
-          },
-        ],
-        true,
-      )
+      fs.readFileSync(join(process.cwd(), 'certificates', 'ca.cert')),
+      [
+        {
+          private_key: fs.readFileSync(
+            join(process.cwd(), 'certificates', 'service.key'),
+          ),
+          cert_chain: fs.readFileSync(
+            join(process.cwd(), 'certificates', 'service.pem'),
+          ),
+        },
+      ],
+      true,
+    ),
   },
 };
